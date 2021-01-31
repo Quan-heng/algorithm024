5acc7b6b7370fde69328db46d03f87d4e1153408
quan <15364446581@163.com>
2021-01-31 11:31:14 +0800

    旋转数组：暴力法

---
+++ Week_01/189旋转数组.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+        if(nums==null){
+            return;
+        }
+        int[] tempArr = new int[nums.length];
+        int step = 0;
+        for (int i = 0; i < nums.length; i++) {
+            step = i+k;
+            if(step>=nums.length){
+                step = (step- nums.length)% nums.length;
+            }
+            tempArr[step] = nums[i];
+        }
+        for (int i = 0; i < tempArr.length; i++) {
+            nums[i] = tempArr[i];
+        }
+    }
+}
