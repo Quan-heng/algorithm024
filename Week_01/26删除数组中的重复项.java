affc74b50301ef3902d5eae23f6b4c94ec3b0cb6
quan <15364446581@163.com>
2021-01-31 10:13:50 +0800

    删除数组中的重复项

---
+++ Week_01/26删除数组中的重复项.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int index = 0;
+        if (nums == null) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return 1;
+        }
+        for (int i = 1; i < nums.length; i++) {
+            if(nums[i]==nums[i-1]){
+                continue;
+            }else{
+                index++;
+                if(index!=i){
+                    nums[index] = nums[i];
+                }
+            }
+        }
+        return index+1;
+    }
+}
