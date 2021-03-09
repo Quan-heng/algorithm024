571c5c3832df07821a49e7506f397bd0c2adc88d
quanheng <15364446581@163.com>
2021-03-09 23:41:38 +0800

    第4周作业

---
+++ Week_05/palindromic_substrings.java
@@ -0,0 +1,10 @@
+public class palindromic_substrings {
+    public int countSubstrings(String s){
+        int len = s.length();
+        for(int i=0;i<len;i++){
+            for(int j=i;j<len;j++){
+
+            }
+        }
+    }
+}
