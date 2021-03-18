ce665594f212308c50afb6c488db6288366ac1a0
heng <15364446581@163.com>
2021-03-18 15:02:49 +0800

    双向BFS

---
+++ Week_07/word_ladder.java
@@ -0,0 +1,52 @@
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class word_ladder {
+    public static int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> wordSet = new HashSet<>(wordList);
+        if (!wordSet.contains(endWord)) return 0;
+        Set<String> beginSet = new HashSet<>();
+        beginSet.add(beginWord);
+        Set<String> endSet = new HashSet<>();
+        endSet.add(endWord);
+        Set<String> visited = new HashSet<>();
+        int result = 0;
+        while (!beginSet.isEmpty() && !endSet.isEmpty()) {
+            if (beginSet.size() > endSet.size()) {
+                Set<String> temp = beginSet;
+                beginSet = endSet;
+                endSet = temp;
+            }
+            Set<String> newSet = new HashSet<>();
+            for (String word : beginSet) {
+                if (endSet.contains(word)) return result + 1;
+                visited.add(word);
+                char[] charArray = word.toCharArray();
+                for (int i = 0; i < charArray.length; i++) {
+                    char old = charArray[i];
+                    for (char j = 'a'; j <= 'z'; j++) {
+                        if (j == old) continue;
+                        charArray[i] = j;
+                        String str = String.valueOf(charArray);
+                        if (wordSet.contains(str) && !visited.contains(str)) {
+                            newSet.add(str);
+                        }
+                    }
+                    charArray[i] = old;
+                }
+            }
+            beginSet = newSet;
+            result++;
+        }
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String begin = "hit";
+        String end = "cog";
+        String[] wordArr = new String[]{"hot","dot","dog","lot","log","cog"};
+        System.out.println(ladderLength(begin,end, Arrays.asList(wordArr)));
+    }
+}
