421227246a80d1da7d3e0a1741d452b33703567a
heng <15364446581@163.com>
2021-04-06 09:14:40 +0800

    n皇后

--- Week_08/power_of_two.java
+++ Week_08/power_of_two.java
@@ -1,6 +1,6 @@
 public class power_of_two {
     public boolean isPowerOfTwo(int n) {
         long num = n;
-        return num!=0&&(num&(num-1))==0;
+        return num != 0 && (num & (num - 1)) == 0;
     }
 }
